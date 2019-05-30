1c7e06ef40cc8386d602a8b67ee5a811ca7edca8
Tom White <eugene.white@gmail.com>
2019-05-30 04:23:55 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    BRANDS.include?(brand) ? true : BRANDS << brand
+    BRANDS.include?(brand) ? : BRANDS << brand
   end
 
   def cobble
